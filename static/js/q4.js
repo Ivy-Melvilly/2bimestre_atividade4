955f245e5c28cb3f52e75c5dbc1b58b2ac187ca4
Ivy Melvilly <108835965+Ivy-Melvilly@users.noreply.github.com>
2023-08-01 17:41:00 +0000

    link das imagens da questão 4

--- static/js/q4.js
+++ static/js/q4.js
@@ -1,13 +1,13 @@
 function mudarForma() {
     var forma = document.getElementById("forma");
     if (forma.value == "triangulo") {
-      document.querySelector('img').src = "imagens/triangulo.png";
+      document.querySelector('img').src = "static/imagens/triangulo.png";
     } else if (forma.value == "quadrado") {
-      document.querySelector('img').src = "imagens/quadrado.png";
+      document.querySelector('img').src = "static/imagens/quadrado.png";
     } else if (forma.value == "circulo") {
-      document.querySelector('img').src = "imagens/circulo.png";
+      document.querySelector('img').src = "static/imagens/circulo.png";
     } else {
-      document.querySelector('img').src = "imagens/interrogacao.png";
+      document.querySelector('img').src = "static/imagens/interrogacao.png";
       alert("forma inválida!")
     }
   }
