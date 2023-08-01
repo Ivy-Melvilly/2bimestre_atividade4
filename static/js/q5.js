eb3f3fb6ed704f4d37a2c3af639d8d86e5c511ce
Ivy Melvilly <108835965+Ivy-Melvilly@users.noreply.github.com>
2023-08-01 20:06:26 +0000

    url da questão 5

--- static/js/q5.js
+++ static/js/q5.js
@@ -1,6 +1,6 @@
 function imagens() {
-    var lista = ["imagens/bougainville.jpg", "imagens/camelia.jpg", "imagens/crisantemo.jpg", "imagens/flor-de-cerejeira.jpg", "imagens/hibisco.jpg", "imagens/hortensia.jpg", "imagens/lirio-do-vale.jpg", "imagens/lotus.jpg", "imagens/orquidea.jpg", "imagens/rosas.jpg"]; 
-    for (i = 0; i < lista.length; i++) {
-      
+    var f = ["imagens/bougainville.jpg", "imagens/camelia.jpg", "imagens/crisantemo.jpg", "imagens/flor-de-cerejeira.jpg", "imagens/hibisco.jpg", "imagens/hortensia.jpg", "imagens/lirio-do-vale.jpg", "imagens/lotus.jpg", "imagens/orquidea.jpg", "imagens/rosas.jpg"]; 
+    for (i = 0; i < f.length; i++) {
+      document.getElementById('f').innerHTML+= "<img src='static/imagens/"+f[i]+".jpg' >"
     }
   }
